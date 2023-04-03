deb55a89cf55b0d9b2d34fc3767bc4d4e157194e
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-04-03 17:49:35 +0200

    Rozwiązanie.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,14 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Room room1 = new Room(15, 17, true);
+        room1.setTheSmallestTemperature(15);
+        System.out.println("Aktualna temperatura: " + room1.getTemperature());
+        boolean cold = room1.temperatureDown();
+        System.out.println("Czy udało się obniżyć temperaturę ? " + cold + "\naktualna temperatura wynosi: " + room1.getTemperature());
+        boolean cold1 = room1.temperatureDown();
+        System.out.println("Czy udało się obniżyć temperaturę ? " + cold1 + "\naktualna temperatura wynosi: " + room1.getTemperature());
+        boolean cold2 = room1.temperatureDown();
+        System.out.println("Czy udało się obniżyć temperaturę ? " + cold2 + "\naktualna temperatura wynosi: " + room1.getTemperature());
     }
 }
---
+++ src/main/java/pl/javastart/task/Room.java
@@ -0,0 +1,31 @@
+package pl.javastart.task;
+
+public class Room {
+    private final double meters;
+    private double temperature;
+    private double theSmallestTemperature;
+    private final boolean airConditioner;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTheSmallestTemperature(double theSmallestTemperature) {
+        this.theSmallestTemperature = theSmallestTemperature;
+    }
+
+    public Room(double meters, double temperature, boolean airConditioner) {
+        this.meters = meters;
+        this.temperature = temperature;
+        this.airConditioner = airConditioner;
+    }
+
+    public boolean temperatureDown() {
+        if ((airConditioner && (temperature - 1 >= theSmallestTemperature))) {
+            temperature--;
+            return true;
+        }
+        return false;
+    }
+}
+
